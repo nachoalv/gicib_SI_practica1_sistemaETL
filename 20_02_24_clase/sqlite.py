8b0c96bd339cd9bc76a078c54a0c43c53316b4a2
d-montero <d.montero.2021@alumnos.urjc.es>
2024-02-25 19:06:59 +0100

    last columns
    tables for last two columns
    repes

--- 20_02_24_clase/sqlite.py
+++ 20_02_24_clase/sqlite.py
@@ -23,21 +23,47 @@ cur.execute('''CREATE TABLE usuarios (  nombre      TEXT,
                                         emails_clicados INTEGER,
                                         PRIMARY KEY(nombre))''')
 
+cur.execute('''DROP TABLE IF EXISTS fechas''')
+cur.execute('''CREATE TABLE "fechas" (  "fecha"     TEXT,
+                                        "usuario"   TEXT,
+	                                    FOREIGN KEY("usuario") REFERENCES "usuarios"("nombre") ON UPDATE CASCADE ON DELETE CASCADE,
+	                                    PRIMARY KEY("fecha", "usuario"))''')
+
+cur.execute('''DROP TABLE IF EXISTS ips''')
+cur.execute('''CREATE TABLE "ips" ( "ip"     TEXT,
+                                    "usuario"   TEXT,
+	                                FOREIGN KEY("usuario") REFERENCES "usuarios"("nombre") ON UPDATE CASCADE ON DELETE CASCADE,
+	                                PRIMARY KEY("ip", "usuario"))''')
 
 f = open('../data/legal_data_online.json', 'r')
 legal_data = json.load(f)
 for item in legal_data["legal"]:
     key = list(item.keys())[0]
     web = item[key]
-    cur.execute('''INSERT INTO legal (nombre, cookies, aviso, proteccion_de_datos, creacion) VALUES (?, ?, ?, ?, ?)''', (key, web['cookies'],web['aviso'], web['proteccion_de_datos'],web['creacion']))
+    cur.execute('''INSERT INTO legal (nombre, cookies, aviso, proteccion_de_datos, creacion) VALUES (?, ?, ?, ?, ?)''',
+                (key, web['cookies'], web['aviso'], web['proteccion_de_datos'], web['creacion']))
 
 f = open('../data/users_data_online.json', 'r')
 users_data = json.load(f)
 for item in users_data["usuarios"]:
     key = list(item.keys())[0]
     user = item[key]
-    cur.execute('''INSERT INTO usuarios (nombre, telefono, contrasena, provincia, permisos, emails_total, emails_phishing, emails_clicados) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', (key, user['telefono'], user['contrasena'], user['provincia'], user['permisos'], user['emails']['total'], user['emails']['phishing'], user['emails']['cliclados']))
-
+    cur.execute(
+        '''INSERT INTO usuarios (nombre, telefono, contrasena, provincia, permisos, emails_total, emails_phishing, emails_clicados) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+        (key, user['telefono'], user['contrasena'], user['provincia'], user['permisos'], user['emails']['total'],
+         user['emails']['phishing'], user['emails']['cliclados']))
+    for fecha in user['fechas']:
+        try:
+            cur.execute('''INSERT INTO fechas (fecha, usuario) VALUES (?, ?)''',
+                        (fecha, key))
+        except sqlite3.IntegrityError:
+            print("repetido: "+key+" "+fecha)
+    for ip in user['ips']:
+        try:
+            cur.execute('''INSERT INTO ips (ip, usuario) VALUES (?, ?)''',
+                        (ip, key))
+        except sqlite3.IntegrityError:
+            print("repetido: "+key+" "+ip)
 
 con.commit()
 con.close()
