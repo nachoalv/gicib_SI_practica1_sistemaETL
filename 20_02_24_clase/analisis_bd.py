45feb083ecf26eb19103900508adce4be6665b8a
nachoalv <ignacioalvaneznunez@gmail.com>
2024-02-27 18:22:45 +0100

    layout + updated analisis
    Co-Authored-By: Almeida <72100006+aalmeidar@users.noreply.github.com>
    Co-Authored-By: d-montero <126509755+d-montero@users.noreply.github.com>
    Co-Authored-By: José Manuel Jerónimo Rodríguez <126869573+joseemjr@users.noreply.github.com>

--- 20_02_24_clase/analisis_bd.py
+++ 20_02_24_clase/analisis_bd.py
@@ -9,13 +9,15 @@ con = sqlite3.connect("gicib_SI_practica1_sistemaETL.db")
 query_num_muestras = "SELECT COUNT(*) FROM usuarios WHERE telefono IS NOT NULL AND provincia IS NOT NULL"
 query_fechas = "SELECT COUNT(*) FROM cambio_psw GROUP BY usuario"
 query_ips = "SELECT COUNT(DISTINCT ip) FROM cambio_psw GROUP BY usuario"
-query_emails_phishing = "SELECT emails_phishing FROM usuarios"
-query_emails_total = "SELECT emails_total FROM usuarios"
+query_emails_phishing = "SELECT emails_clicados FROM usuarios" #WHERE telefono IS NOT NULL AND provincia IS NOT NULL"
+query_emails_total = "SELECT emails_total FROM usuarios" # WHERE telefono IS NOT NULL AND provincia IS NOT NULL"
+query_emails_phishing_admin = "SELECT emails_clicados FROM usuarios WHERE permisos = 1"# AND telefono IS NOT NULL AND provincia IS NOT NULL"
 
 num_muestras = pd.read_sql_query(query_num_muestras, con).iloc[0, 0]
 fechas_cambio_psw = pd.read_sql_query(query_fechas, con)
 ips_detectadas = pd.read_sql_query(query_ips, con)
 emails_phishing = pd.read_sql_query(query_emails_phishing, con)
+emails_phishing_admin = pd.read_sql_query(query_emails_phishing_admin  , con)
 emails_total = pd.read_sql_query(query_emails_total, con)
 
 media_fechas = fechas_cambio_psw.mean().values[0]
@@ -28,6 +30,8 @@ desviacion_emails_phishing = emails_phishing.std().values[0]
 
 min_emails_total = emails_total.min().values[0]
 max_emails_total = emails_total.max().values[0]
+min_emails_admin = emails_phishing_admin.min().values[0]
+max_emails_admin = emails_phishing_admin.max().values[0]
 
 con.close()
 
@@ -37,7 +41,7 @@ print("Desviación estándar del total de fechas en las que se ha cambiado la co
 print("Media del total de IPs que se han detectado:", media_ips)
 print("Desviación estándar del total de IPs que se han detectado:", desviacion_ips)
 print("Media del número de email recibidos de phishing:", media_emails_phishing)
-print("Desviación estándar del número de email recibidos de phishing:", desviacion_emails_phishing)
+print("Desviación estándar del número de email recibidos de phishing interactuados:", desviacion_emails_phishing)
 print("Valor mínimo del total de emails recibidos:", min_emails_total)
 print("Valor máximo del total de emails recibidos:", max_emails_total)
 
