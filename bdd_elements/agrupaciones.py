15dd698662fbdb85ab1449fd5f3f29d08edbc35d
José Manuel Jerónimo Rodríguez <jm.jeronimo.2021@alumnos.urjc.es>
2024-02-29 14:14:18 +0100

    Fix

--- bdd_elements/agrupaciones.py
+++ bdd_elements/agrupaciones.py
@@ -27,33 +27,27 @@ def calcular_info_por_agrupacion(data):
     return num_observaciones, num_valores_ausentes, mediana, media, varianza, maximo, minimo
 
 
+if __name__ == '__main__':
+    con = sqlite3.connect("gicib_SI_practica1_sistemaETL.db")
+    passwds_debiles = set(hash_md5(passwd) for passwd in cargar_wordlist('../data/SmallRockYou.txt'))
 
-con = sqlite3.connect("gicib_SI_practica1_sistemaETL.db")
-passwds_debiles = set(hash_md5(passwd) for passwd in cargar_wordlist('SmallRockYou.txt'))
+    query = '''
+        SELECT permisos, emails_phishing, contrasena
+        FROM usuarios
+        WHERE emails_phishing IS NOT NULL
+    '''
 
-query = '''
-    SELECT permisos, emails_phishing, contrasena
-    FROM usuarios
-    WHERE emails_phishing IS NOT NULL
-'''
+    df = pd.read_sql_query(query, con)
+    con.close()
 
-df = pd.read_sql_query(query, con)
-con.close()
-
-for permisos, group in df.groupby('permisos'):
-    if permisos == 0:
-        tipo_usuario = 'Usuario'
-    elif permisos == 1:
-        tipo_usuario = 'Administrador'
-    print(f"\nAgrupacion: {tipo_usuario}")
-    for passwd_debil, group_c in group.groupby(
-            df.apply(lambda row: es_passwd_debil(row['contrasena'], passwds_debiles), axis=1)):
-        if passwd_debil:
-            tipo_passwd = 'Débil'
-        else:
-            tipo_passwd = 'Fuerte'
-        print(f"\nTipo de contraseña: {tipo_passwd}")
-        info = calcular_info_por_agrupacion(group_c['emails_phishing'])
+    print('='*30+'Agrupaciones SEPARADAS'+'='*30)
+    for permisos, group in df.groupby('permisos'):
+        if permisos == 0:
+            tipo_usuario = 'Usuario'
+        elif permisos == 1:
+            tipo_usuario = 'Administrador'
+        print(f"\nAgrupacion: {tipo_usuario}")
+        info = calcular_info_por_agrupacion(group['emails_phishing'])
         print("Numero de observaciones:", info[0])
         print("Numero de valores ausentes (missing):", info[1])
         print("Mediana:", info[2])
@@ -61,3 +55,43 @@ for permisos, group in df.groupby('permisos'):
         print("Varianza:", info[4])
         print("Valor máximo:", info[5])
         print("Valor mínimo:", info[6])
+
+
+    for passwd_debil, group in df.groupby(
+            df.apply(lambda row: es_passwd_debil(row['contrasena'], passwds_debiles), axis=1)):
+        if passwd_debil:
+            tipo_passwd = 'Débil'
+        else:
+            tipo_passwd = 'Fuerte'
+        print(f"\nTipo de contraseña: {tipo_passwd}")
+        info = calcular_info_por_agrupacion(group['emails_phishing'])
+        print("Numero de observaciones:", info[0])
+        print("Numero de valores ausentes (missing):", info[1])
+        print("Mediana:", info[2])
+        print("Media:", info[3])
+        print("Varianza:", info[4])
+        print("Valor máximo:", info[5])
+        print("Valor mínimo:", info[6])
+
+    print('='*30+'Agrupaciones COMBINADAS'+'='*30)
+    for permisos, group in df.groupby('permisos'):
+        if permisos == 0:
+            tipo_usuario = 'Usuario'
+        elif permisos == 1:
+            tipo_usuario = 'Administrador'
+        print(f"\nAgrupacion: {tipo_usuario}")
+        for passwd_debil, group_c in group.groupby(
+                df.apply(lambda row: es_passwd_debil(row['contrasena'], passwds_debiles), axis=1)):
+            if passwd_debil:
+                tipo_passwd = 'Débil'
+            else:
+                tipo_passwd = 'Fuerte'
+            print(f"\nTipo de contraseña: {tipo_passwd}")
+            info = calcular_info_por_agrupacion(group_c['emails_phishing'])
+            print("Numero de observaciones:", info[0])
+            print("Numero de valores ausentes (missing):", info[1])
+            print("Mediana:", info[2])
+            print("Media:", info[3])
+            print("Varianza:", info[4])
+            print("Valor máximo:", info[5])
+            print("Valor mínimo:", info[6])
--- bdd_elements/insercion_bdd.py
+++ bdd_elements/insercion_bdd.py
@@ -1,74 +1,75 @@
-# W/ code with me
 import json
 import sqlite3
 
-con = sqlite3.connect("gicib_SI_practica1_sistemaETL.db")
-cur = con.cursor()
-cur.execute('''DROP TABLE IF EXISTS legal ''')
-cur.execute('''CREATE TABLE legal ( nombre      TEXT,
-                                    cookies	    INTEGER NOT NULL CHECK(cookies in (0,1)),
-                                    aviso       INTEGER NOT NULL CHECK(aviso in (0,1)),
-                                    proteccion_de_datos	INTEGER NOT NULL CHECK(proteccion_de_datos in (0,1)),
-                                    creacion	INTEGER NOT NULL,
-                                    PRIMARY KEY(nombre))''')
+if __name__ == '__main__':
 
-cur.execute('''DROP TABLE IF EXISTS usuarios''')
-cur.execute('''CREATE TABLE usuarios (  nombre      TEXT,
-                                        telefono    INTEGER,
-                                        contrasena  TEXT NOT NULL,
-                                        provincia	TEXT,
-                                        permisos	INTEGER NOT NULL CHECK(permisos in (0,1)),
-                                        emails_total INTEGER NOT NULL,
-                                        emails_phishing INTEGER NOT NULL,
-                                        emails_clicados INTEGER NOT NULL,
+    con = sqlite3.connect("gicib_SI_practica1_sistemaETL.db")
+    cur = con.cursor()
+    cur.execute('''DROP TABLE IF EXISTS legal ''')
+    cur.execute('''CREATE TABLE legal ( nombre      TEXT,
+                                        cookies	    INTEGER NOT NULL CHECK(cookies in (0,1)),
+                                        aviso       INTEGER NOT NULL CHECK(aviso in (0,1)),
+                                        proteccion_de_datos	INTEGER NOT NULL CHECK(proteccion_de_datos in (0,1)),
+                                        creacion	INTEGER NOT NULL,
                                         PRIMARY KEY(nombre))''')
 
-
-cur.execute('''DROP TABLE IF EXISTS cambio_psw''')
-cur.execute('''CREATE TABLE "cambio_psw" ( "id"        INTEGER,
-                                        "fecha"     TEXT NOT NULL,
-                                        "ip"        TEXT,
-                                        "usuario"   TEXT NOT NULL,
-	                                    FOREIGN KEY("usuario") REFERENCES "usuarios"("nombre") ON UPDATE CASCADE ON DELETE CASCADE,
-	                                    PRIMARY KEY("id" AUTOINCREMENT))''')
-
-f = open('../data/legal_data_online.json', 'r')
-legal_data = json.load(f)
-for item in legal_data["legal"]:
-    key = list(item.keys())[0]
-    web = item[key]
-    cur.execute('''INSERT INTO legal (nombre, cookies, aviso, proteccion_de_datos, creacion) VALUES (?, ?, ?, ?, ?)''',
-                (key, web['cookies'], web['aviso'], web['proteccion_de_datos'], web['creacion']))
+    cur.execute('''DROP TABLE IF EXISTS usuarios''')
+    cur.execute('''CREATE TABLE usuarios (  nombre      TEXT,
+                                            telefono    INTEGER,
+                                            contrasena  TEXT NOT NULL,
+                                            provincia	TEXT,
+                                            permisos	INTEGER NOT NULL CHECK(permisos in (0,1)),
+                                            emails_total INTEGER NOT NULL,
+                                            emails_phishing INTEGER NOT NULL,
+                                            emails_clicados INTEGER NOT NULL,
+                                            PRIMARY KEY(nombre))''')
 
 
-f = open('../data/users_data_online.json', 'r')
-users_data = json.load(f)
-for item in users_data["usuarios"]:
-    key = list(item.keys())[0]
-    user = item[key]
-    if user['telefono'] == 'None':
-        tlf = None
-    else:
-        tlf = user['telefono']
-    if user['provincia'] == 'None':
-        prv = None
-    else:
-        prv = user['provincia']
-    cur.execute(
-        '''INSERT INTO usuarios (nombre, telefono, contrasena, provincia, permisos, emails_total, emails_phishing, emails_clicados) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
-        (key, tlf, user['contrasena'], prv, user['permisos'], user['emails']['total'],
-         user['emails']['phishing'], user['emails']['cliclados']))
-    for i in range(len(user['fechas'])):
-        if user['ips'] == 'None':
-            ips = None
+    cur.execute('''DROP TABLE IF EXISTS cambio_psw''')
+    cur.execute('''CREATE TABLE "cambio_psw" ( "id"        INTEGER,
+                                            "fecha"     TEXT NOT NULL,
+                                            "ip"        TEXT,
+                                            "usuario"   TEXT NOT NULL,
+                                            FOREIGN KEY("usuario") REFERENCES "usuarios"("nombre") ON UPDATE CASCADE ON DELETE CASCADE,
+                                            PRIMARY KEY("id" AUTOINCREMENT))''')
+
+    f = open('../data/legal_data_online.json', 'r')
+    legal_data = json.load(f)
+    for item in legal_data["legal"]:
+        key = list(item.keys())[0]
+        web = item[key]
+        cur.execute('''INSERT INTO legal (nombre, cookies, aviso, proteccion_de_datos, creacion) VALUES (?, ?, ?, ?, ?)''',
+                    (key, web['cookies'], web['aviso'], web['proteccion_de_datos'], web['creacion']))
+
+
+    f = open('../data/users_data_online.json', 'r')
+    users_data = json.load(f)
+    for item in users_data["usuarios"]:
+        key = list(item.keys())[0]
+        user = item[key]
+        if user['telefono'] == 'None':
+            tlf = None
         else:
-            ips = user['ips'][i]
+            tlf = user['telefono']
+        if user['provincia'] == 'None':
+            prv = None
+        else:
+            prv = user['provincia']
+        cur.execute(
+            '''INSERT INTO usuarios (nombre, telefono, contrasena, provincia, permisos, emails_total, emails_phishing, emails_clicados) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+            (key, tlf, user['contrasena'], prv, user['permisos'], user['emails']['total'],
+             user['emails']['phishing'], user['emails']['cliclados']))
+        for i in range(len(user['fechas'])):
+            if user['ips'] == 'None':
+                ips = None
+            else:
+                ips = user['ips'][i]
 
-        fecha = user['fechas'][i] if 'fechas' in user and i < len(user['fechas']) else None
-        ip = user['ips'][i] if 'ips' in user and i < len(user['ips']) else None
+            fecha = user['fechas'][i] if 'fechas' in user and i < len(user['fechas']) else None
+            ip = user['ips'][i] if 'ips' in user and i < len(user['ips']) else None
 
-        cur.execute('''INSERT INTO cambio_psw (fecha, ip, usuario) VALUES (?, ?, ?)''',
-                    (user['fechas'][i], ips, key))
+            cur.execute('''INSERT INTO cambio_psw (fecha, ip, usuario) VALUES (?, ?, ?)''',
+                        (user['fechas'][i], ips, key))
 
-con.commit()
-con.close()
+    con.commit()
+    con.close()
