2df2d1677c0a4fc4fead0a7f224e76de43ed2c15
José Manuel Jerónimo Rodríguez <jm.jeronimo.2021@alumnos.urjc.es>
2024-02-29 11:46:38 +0100

    Ej3

---
+++ bdd_elements/agrupaciones.py
@@ -0,0 +1,63 @@
+import sqlite3
+import pandas as pd
+import hashlib
+
+
+def cargar_wordlist(diccionario):
+    with open(diccionario, 'r', encoding='latin-1') as f:
+        return [line.strip() for line in f]
+
+
+def hash_md5(passwd):
+    return hashlib.md5(passwd.encode()).hexdigest()
+
+
+def es_passwd_debil(hash_passwd, passwds_debiles):
+    return hash_passwd in passwds_debiles
+
+
+def calcular_info_por_agrupacion(data):
+    num_observaciones = data.count()
+    num_valores_ausentes = data.isnull().sum()
+    mediana = data.median()
+    media = data.mean()
+    varianza = data.var()
+    maximo = data.max()
+    minimo = data.min()
+    return num_observaciones, num_valores_ausentes, mediana, media, varianza, maximo, minimo
+
+
+
+con = sqlite3.connect("gicib_SI_practica1_sistemaETL.db")
+passwds_debiles = set(hash_md5(passwd) for passwd in cargar_wordlist('SmallRockYou.txt'))
+
+query = '''
+    SELECT permisos, emails_phishing, contrasena
+    FROM usuarios
+    WHERE emails_phishing IS NOT NULL
+'''
+
+df = pd.read_sql_query(query, con)
+con.close()
+
+for permisos, group in df.groupby('permisos'):
+    if permisos == 0:
+        tipo_usuario = 'Usuario'
+    elif permisos == 1:
+        tipo_usuario = 'Administrador'
+    print(f"\nAgrupacion: {tipo_usuario}")
+    for passwd_debil, group_c in group.groupby(
+            df.apply(lambda row: es_passwd_debil(row['contrasena'], passwds_debiles), axis=1)):
+        if passwd_debil:
+            tipo_passwd = 'Débil'
+        else:
+            tipo_passwd = 'Fuerte'
+        print(f"\nTipo de contraseña: {tipo_passwd}")
+        info = calcular_info_por_agrupacion(group_c['emails_phishing'])
+        print("Numero de observaciones:", info[0])
+        print("Numero de valores ausentes (missing):", info[1])
+        print("Mediana:", info[2])
+        print("Media:", info[3])
+        print("Varianza:", info[4])
+        print("Valor máximo:", info[5])
+        print("Valor mínimo:", info[6])
--- bdd_elements/analisis_bd.py
+++ bdd_elements/analisis_bd.py
@@ -1,24 +1,54 @@
 import sqlite3
 import pandas as pd
 
-import sqlite3
-import pandas as pd
 
 con = sqlite3.connect("gicib_SI_practica1_sistemaETL.db")
 
-query_num_muestras = "SELECT COUNT(*) FROM usuarios WHERE telefono IS NOT NULL AND provincia IS NOT NULL"
-query_fechas = "SELECT COUNT(*) FROM cambio_psw GROUP BY usuario"
-query_ips = "SELECT COUNT(DISTINCT ip) FROM cambio_psw GROUP BY usuario"
-query_emails_phishing = "SELECT emails_clicados FROM usuarios" #WHERE telefono IS NOT NULL AND provincia IS NOT NULL"
-query_emails_total = "SELECT emails_total FROM usuarios" # WHERE telefono IS NOT NULL AND provincia IS NOT NULL"
-query_emails_phishing_admin = "SELECT emails_clicados FROM usuarios WHERE permisos = 1"# AND telefono IS NOT NULL AND provincia IS NOT NULL"
+query_num_muestras = '''
+    SELECT COUNT(*) 
+    FROM usuarios 
+    WHERE telefono IS NOT NULL AND provincia IS NOT NULL
+    '''
+
+query_fechas = '''
+    SELECT COUNT(DISTINCT fecha) AS num_fechas
+    FROM cambio_psw 
+    WHERE usuario IN (SELECT nombre FROM usuarios WHERE telefono IS NOT NULL AND provincia IS NOT NULL) 
+    GROUP BY usuario
+'''
+
+query_ips = '''
+    SELECT COUNT(DISTINCT ip) AS num_ips 
+    FROM cambio_psw 
+    WHERE usuario IN (SELECT nombre FROM usuarios WHERE telefono IS NOT NULL AND provincia IS NOT NULL) 
+    GROUP BY usuario
+'''
+
+query_emails_phishing = '''
+    SELECT emails_clicados 
+    FROM usuarios 
+    WHERE telefono IS NOT NULL AND provincia IS NOT NULL
+'''
+
+query_emails_total = '''
+    SELECT emails_total
+    FROM usuarios 
+    WHERE telefono IS NOT NULL AND provincia IS NOT NULL
+'''
+
+query_emails_phishing_admin = '''
+    SELECT emails_clicados 
+    FROM usuarios 
+    WHERE permisos = 1 AND telefono IS NOT NULL AND provincia IS NOT NULL
+'''
 
 num_muestras = pd.read_sql_query(query_num_muestras, con).iloc[0, 0]
 fechas_cambio_psw = pd.read_sql_query(query_fechas, con)
 ips_detectadas = pd.read_sql_query(query_ips, con)
 emails_phishing = pd.read_sql_query(query_emails_phishing, con)
-emails_phishing_admin = pd.read_sql_query(query_emails_phishing_admin  , con)
+emails_phishing_admin = pd.read_sql_query(query_emails_phishing_admin, con)
 emails_total = pd.read_sql_query(query_emails_total, con)
+con.close()
 
 media_fechas = fechas_cambio_psw.mean().values[0]
 media_ips = ips_detectadas.mean().values[0]
@@ -33,16 +63,14 @@ max_emails_total = emails_total.max().values[0]
 min_emails_admin = emails_phishing_admin.min().values[0]
 max_emails_admin = emails_phishing_admin.max().values[0]
 
-con.close()
-
 print("Número de muestras:", num_muestras)
 print("Media del total de fechas en las que se ha cambiado la contraseña:", media_fechas)
 print("Desviación estándar del total de fechas en las que se ha cambiado la contraseña:", desviacion_fechas)
 print("Media del total de IPs que se han detectado:", media_ips)
 print("Desviación estándar del total de IPs que se han detectado:", desviacion_ips)
-print("Media del número de email recibidos de phishing:", media_emails_phishing)
-print("Desviación estándar del número de email recibidos de phishing interactuados:", desviacion_emails_phishing)
+print("Media del número de email interactuados de phishing:", media_emails_phishing)
+print("Desviación estándar del número de email interactuados de phishing:", desviacion_emails_phishing)
 print("Valor mínimo del total de emails recibidos:", min_emails_total)
 print("Valor máximo del total de emails recibidos:", max_emails_total)
-
-
+print("Valor mínimo del total de emails de phishing interactuados por admins:", min_emails_admin)
+print("Valor máximo del total de emails de phishing interactuados por admins:", max_emails_admin)
